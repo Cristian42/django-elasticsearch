004911e0dd22c69a0f3d1c50f87290d1b6722724
Cristian Andronic <andronic@princeton.edu>
2016-08-16 13:33:06 -0400

    Added elasticsearch client info to django settings

--- web/settings.py
+++ web/settings.py
@@ -120,3 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+
+from elasticsearch import Elasticsearch, RequestsHttpConnection
+ES_CLIENT = Elasticsearch(
+    ['http://127.0.0.1:9200/'],
+    connection_class=RequestsHttpConnection
+)
