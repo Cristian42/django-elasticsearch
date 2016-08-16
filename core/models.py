158bcdc90160f9c1fcdeeb445b0c5eaad24423ff
Cristian Andronic <andronic@princeton.edu>
2016-08-16 13:29:36 -0400

    Added elasticsearch mapping in Student model Meta class

--- core/models.py
+++ core/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 
+import django.db.models.options as options
+options.DEFAULT_NAMES = options.DEFAULT_NAMES + (
+    'es_index_name', 'es_type_name', 'es_mapping'
+)
+
+
 class University(models.Model):
     name = models.CharField(max_length=255, unique=True)
 class Course(models.Model):
@@ -23,3 +29,34 @@ class Student(models.Model):
     # various relationships models
     university = models.ForeignKey(University, null=True, blank=True)
     courses = models.ManyToManyField(Course, null=True, blank=True)
+
+
+    class Meta:
+        es_index_name = 'django'
+        es_type_name = 'student'
+        es_mapping = {
+            'properties': {
+                'university': {
+                    'type': 'object',
+                    'properties': {
+                        'name': {'type': 'string', 'index': 'not_analyzed'},
+                    }
+                },
+                'first_name': {'type': 'string', 'index': 'not_analyzed'},
+                'last_name': {'type': 'string', 'index': 'not_analyzed'},
+                'age': {'type': 'short'},
+                'year_in_school': {'type': 'string'},
+                'name_complete': {
+                    'type': 'completion',
+                    'analyzer': 'simple',
+                    'payloads': True,
+                    'preserve_separators': True,
+                    'preserve_position_increments': True,
+                    'max_input_length': 50,
+                },
+                "course_names": {
+                    "type": "string", "store": "yes", "index": "not_analyzed",
+                },
+            }
+        }
+
