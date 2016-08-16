394a1b5fc8b0a228d13002045e188f536b4588ea
Cristian Andronic <andronic@princeton.edu>
2016-08-16 13:15:31 -0400

    Hooked up models to admin site

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,6 @@
-from django.contrib import admin
+from django.contrib import admin 
+from .models import University, Course, Student 
 
-# Register your models here.
+admin.site.register(University) 
+admin.site.register(Course) 
+admin.site.register(Student)
